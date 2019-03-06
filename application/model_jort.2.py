a62321121e1c0d89614302b769fb71f0da1edeb8
jortvangorkum <jortvangorkum@gmail.com>
2019-03-06 11:28:02 +0100

    Refactor model_jort

---
+++ application/model_jort.2.py
@@ -0,0 +1,95 @@
+# To build the NN model
+import tensorflow as tf
+from tensorflow import keras
+
+# For logging the training
+import tensorboard as Tensorboard
+
+# Check if directories exists
+import os
+
+# Helper libraries
+import numpy as np
+import csv
+
+class Model:
+  def __init__(self, height:int, width:int, log_path:str, model_path:str):
+    """
+    Settings
+    """
+    self.height = height
+    self.width = width
+    self.size = height * width
+    self.log_path = log_path
+    self.model_path = model_path
+    def determine_model_number(counter):
+      path = f'{self.model_path}/model_jort_{counter}'
+      while (os.path.isfile(path)):
+        counter += 1
+        path = f'{self.model_path}/model_jort_{counter}'
+      return counter
+    self.model_number = determine_model_number(0)
+    """
+    Keras Model
+    """
+    model = keras.Sequential([
+      keras.layers.Dense(self.size, input_dim=self.size, activation=tf.nn.tanh),
+      keras.layers.Dense(self.size, activation=tf.nn.tanh),
+      keras.layers.Dense(self.size, activation=tf.nn.tanh),
+      keras.layers.Dense(self.size, activation=tf.nn.tanh),
+      keras.layers.Dense(1, activation=tf.nn.tanh),
+    ])
+    model.compile(
+      optimizer=tf.train.AdamOptimizer(),
+      loss='mean_squared_error',
+      metrics=['accuracy']
+    )
+    self.model = model
+
+  def retrieve_data(self, input_path:str):
+    # input_path: 'data_from_internet/c4_game_database.csv'
+    with open(f"../data/{input_path}", newline="") as csvfile:
+      data = list(csv.reader(csvfile))
+      return data
+
+  def convert_data(self, data:list):
+    # Deletes Header
+    del data[0]
+
+    # Remove empty spaces
+    data = [list(filter(None, row)) for row in data]
+    # Convert input to float
+    data = [list(map(float, row)) for row in data]
+
+    train_data = [row[:self.size] for row in data]
+    train_data = np.array(train_data)
+
+    test_data = [row[-1] for row in data]
+    # Convert [-1, 0, 1] -> [0, 0,5, 1]
+    test_data =  [(i+1)/2 for i in test_data]
+    test_data = np.array(test_data)
+
+    return (train_data, test_data)
+
+  def create_tensorboard(self):
+    return keras.callbacks.TensorBoard(log_dir=f'{self.log_path}/model_jort_{self.model_number}', histogram_freq=0, write_graph=True, write_images=True)
+
+  def train(self, tensorboard, train_data:list, test_data:list, epochs:int):
+    self.model.fit(train_data, test_data, verbose=1, epochs=epochs, callbacks=[tensorboard], validation_split=0.1)
+
+  def save(self):
+    self.model.save(f"{self.model_path}/model_jort_{self.model_number}")
+
+  def predict(self, train_data:list, test_data:list):
+    predictions = self.model.predict(train_data)
+    estimates = zip(predictions, test_data)
+    map(lambda estimate: print(f"Prediction: {estimate[0]} Real: {estimate[0]}"), estimates)
+
+if __name__ == "__main__":
+  model = Model(6, 7, '../data/model_logs', '../data/models')
+  data = model.retrieve_data('data_from_internet/c4_game_database.csv')
+  (train_data, test_data) = model.convert_data(data)
+  tensorboard = model.create_tensorboard()
+  model.train(tensorboard, train_data, test_data, 1)
+  model.save()
+  model.predict(np.array(train_data[0]), np.array(test_data[0]))
--- application/model_jort.py
+++ application/model_jort.py
@@ -1,31 +1,41 @@
-# To build the NN Model
+# To build the NN model
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import backend as K
 
 # For logging the training
 import tensorboard as Tensorboard
 
-# Check if Directories exists
+# Check if directories exists
 import os
 
 # Helper libraries
 import numpy as np
-
 import csv
 
 """
 Settings
 """
+EPOCHS = 1
 HEIGHT = 6
 WIDTH = 7
 SIZE = HEIGHT * WIDTH
 LOG_PATH = '../data/model_logs'
-MODEL = 0
-# CommandLine: python3 -m tensorboard.main --logdir {LOG_PATH}
-tensorboard = keras.callbacks.TensorBoard(log_dir=f'{LOG_PATH}/model_{MODEL}', histogram_freq=0, write_graph=True, write_images=True)
+MODEL_PATH = '../data/models'
 
-with open('../data/c4_game_database.csv', newline="") as csvfile:
+def DetermineModelNumber(counter):
+  path = f'{MODEL_PATH}/model_jort_{counter}'
+  while (os.path.isdir(path)):
+    counter += 1
+    path = f'{MODEL_PATH}/model_jort_{counter}'
+  return counter
+
+MODEL_NUMBER = DetermineModelNumber(0)
+
+"""
+End of Settings
+"""
+
+with open('../data/data_from_internet/c4_game_database.csv', newline="") as csvfile:
     data = list(csv.reader(csvfile))
 
 # Deletes Header
@@ -37,21 +47,17 @@ data = [list(filter(None, row)) for row in data]
 # Convert input to float
 data = [list(map(float, row)) for row in data]
 
-# TODO convert list to numpy array with shape of (HEIGHT, WIDTH)
-input = [row[:SIZE] for row in data]
-input = np.array(input)
+train_data = [row[:SIZE] for row in data]
+train_data = np.array(train_data)
 
-
-output = [row[-1] for row in data]
-
+test_data = [row[-1] for row in data]
 # Convert [-1, 0, 1] -> [0, 0,5, 1]
-output =  [(i+1)/2 for i in output]
-output = np.array(output)
+test_data =  [(i+1)/2 for i in test_data]
+test_data = np.array(test_data)
 
 """
 Keras Model
 """
-
 # Creating the model
 model = keras.Sequential([
   keras.layers.Dense(SIZE, input_dim=SIZE, activation=tf.nn.tanh),
@@ -67,11 +73,19 @@ model.compile(
   metrics=['accuracy']
 )
 
-model.fit(input, output, verbose=1, epochs=3, callbacks=[tensorboard], validation_split=0.1)
+# Setup tensorboard
+# CommandLine: python3 -m tensorboard.main --logdir {LOG_PATH}
+tensorboard = keras.callbacks.TensorBoard(log_dir=f'{LOG_PATH}/model_jort_{MODEL_NUMBER}', histogram_freq=0, write_graph=True, write_images=True)
+# Train the model with given data
+model.fit(train_data, test_data, verbose=1, epochs=EPOCHS, callbacks=[tensorboard], validation_split=0.1)
 
+# Saving the model
+model.save(f"{MODEL_PATH}/model_jort_{MODEL_NUMBER}")
+
+# To test my model
 def Predict(x):
-  prediction = model.predict(np.array([input[x]]))
-  print(f"Prediction: {prediction} Real: {output[x]}")
+  prediction = model.predict(np.array([train_data[x]]))
+  print(f"Prediction: {prediction} Real: {test_data[x]}")
 
 Predict(0)
 Predict(1)
