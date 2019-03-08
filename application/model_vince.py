5f7680ad3a8b3eab14f48fd1cc2e1ce33b1f7cd7
Vince van Noort <vincevannoort@gmail.com>
2019-03-08 16:05:22 +0100

    updated model to reflect changes in data

--- application/model_vince.py
+++ application/model_vince.py
@@ -4,7 +4,6 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from ast import literal_eval
 
-from tensorflow.python.client import device_lib
 width = 7
 height = 6
 inputs = width * height * 3
@@ -14,10 +13,10 @@ inputs = width * height * 3
 Parse data
 """
 print(f'parse data.')
-with open('../data/data_generated/connect-four-game-10000.txt') as file:
+with open('../data/data_generated/data_row_classify_connect_four_game_10000.txt') as file:
     content = file.readlines()
 
-data = [line.strip().split(";") for line in content] 
+data = [line.strip().split(";") for line in content[1:]] 
 test_data = np.array([np.array(literal_eval(data_item[0])) for data_item in data])
 test_labels = np.array([int(data_item[2]) for data_item in data])
 test_labels_one_hot = np.zeros((test_labels.size, 7))
@@ -41,17 +40,16 @@ class Connect4KerasModel:
     self.model.compile(optimizer=keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
   def train(self, test_data, test_labels_one_hot):
-    self.model.fit(test_data, test_labels_one_hot, epochs=10, batch_size=32)
+    self.model.fit(test_data, test_labels_one_hot, epochs=25, batch_size=32)
 
-  def predict(self, board: Board)
 
-# print(f'data & model ready, start training.')
-# model.compile(optimizer=keras.optimizers.Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
-# # print(model.evaluate(test_data, test_labels_one_hot))
-# input = test_data[10]
-# prediction = model.predict(np.array([input,]))
-# print(prediction)
-# print(np.argmax(prediction))
+print(f'data & model ready, start training.')
+connect_four_model = Connect4KerasModel()
+connect_four_model.train(test_data, test_labels_one_hot)
+input = test_data[0]
+prediction = connect_four_model.model.predict(np.array([input,]))
+print(prediction)
+print(np.argmax(prediction))
 
 # print(np.argmax(predicted[0]))
 # # keras.backend.one_hot(input, num_classes)
