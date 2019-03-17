69c0f3f324e67ef80e34e80ea91cf0e4427e9fab
jortvangorkum <jortvangorkum@gmail.com>
2019-03-17 14:31:35 +0100

    Refactoring connect four

--- application/connect_four.py
+++ application/connect_four.py
@@ -7,7 +7,7 @@ class Player:
     self.signature = signature
 
   def __str__(self):
-    return f'{self.name}'
+    return self.name
 
 class Board:
   def __init__(self):
@@ -17,10 +17,12 @@ class Board:
 
   def get_hole(self, x: int, y: int):
     if x >= self.width or x < 0 or y >= self.height or y < 0:
-      raise Exception(f'Index ({x},{y}) is outside of the connect four board.')
+      raise Exception(f"Index ({x},{y}) is outside of the connect four board.")
     return self.holes[x + (y * self.width)]
 
   def set_hole(self, x: int, y: int, player: Player):
+    if x >= self.width or x < 0 or y >= self.height or y < 0:
+      raise Exception(f"Index ({x},{y}) is outside of the connect four board.")
     self.holes[x + (y * self.width)] = player.signature
 
   def set_column(self, column: int, player: Player):
@@ -30,17 +32,17 @@ class Board:
 
   def get_possible_columns(self):
     possible_columns = []
-    for column in range(7):
+    for column in range(self.width):
       if self.get_hole(column, 0) is 0:
         possible_columns.append(column)
     return possible_columns
-  
+
   def __str__(self):
     board_representation = ""
     for y in range(self.height):
       for x in range(self.width):
         signature = self.get_hole(x, y)
-        board_representation += f'({signature if signature is not 0 else " "})'
+        board_representation += f"({signature if signature is not 0 else ' '})"
       board_representation += '\n'
     return board_representation
 
@@ -51,51 +53,54 @@ class ConnectFour:
     self.second_player = second_player
     self.current_player = choice([first_player, second_player])
 
+  def switch_player(self, player: Player):
+    return self.second_player if self.current_player is self.first_player else self.first_player
+
   def has_won(self):
     # reference: https://stackoverflow.com/questions/29949169/python-connect-4-check-win-function
     board_width = self.board.width
     board_height = self.board.height
-    player = self.second_player if self.current_player is self.first_player else self.first_player
+    player = self.switch_player(self.current_player)
     player_signature = player.signature
 
     # check horizontal spaces
     for x in range(board_width - 3):
       for y in range(board_height):
-        if (self.board.get_hole(x, y) == player_signature 
-        and self.board.get_hole(x+1, y) == player_signature 
-        and self.board.get_hole(x+2, y) == player_signature 
+        if (self.board.get_hole(x, y) == player_signature
+        and self.board.get_hole(x+1, y) == player_signature
+        and self.board.get_hole(x+2, y) == player_signature
         and self.board.get_hole(x+3, y) == player_signature):
           return player
 
     # check vertical spaces
     for x in range(board_width):
       for y in range(board_height - 3):
-        if (self.board.get_hole(x, y) == player_signature 
-        and self.board.get_hole(x, y+1) == player_signature 
-        and self.board.get_hole(x, y+2) == player_signature 
+        if (self.board.get_hole(x, y) == player_signature
+        and self.board.get_hole(x, y+1) == player_signature
+        and self.board.get_hole(x, y+2) == player_signature
         and self.board.get_hole(x, y+3) == player_signature):
           return player
 
     # check / diagonal spaces
     for x in range(board_width - 3):
       for y in range(3, board_height):
-        if (self.board.get_hole(x, y) == player_signature 
-        and self.board.get_hole(x+1, y-1) == player_signature 
-        and self.board.get_hole(x+2, y-2) == player_signature 
+        if (self.board.get_hole(x, y) == player_signature
+        and self.board.get_hole(x+1, y-1) == player_signature
+        and self.board.get_hole(x+2, y-2) == player_signature
         and self.board.get_hole(x+3, y-3) == player_signature):
           return player
 
     # check \ diagonal spaces
     for x in range(board_width - 3):
       for y in range(board_height - 3):
-        if (self.board.get_hole(x, y) == player_signature 
-        and self.board.get_hole(x+1, y+1) == player_signature 
-        and self.board.get_hole(x+2, y+2) == player_signature 
+        if (self.board.get_hole(x, y) == player_signature
+        and self.board.get_hole(x+1, y+1) == player_signature
+        and self.board.get_hole(x+2, y+2) == player_signature
         and self.board.get_hole(x+3, y+3) == player_signature):
           return player
-    
+
     return None
-  
+
   def is_draw(self):
     return 0 not in self.board.holes
 
@@ -118,13 +123,16 @@ class ConnectFour:
   def move(self, column: int):
     if self.can_move(column):
       self.board.set_column(column, self.current_player)
-    self.current_player = self.second_player if self.current_player is self.first_player else self.first_player
+      self.current_player = self.switch_player(self.current_player)
+    else:
+      print("Board is full. No moves can be set.")
+
 
   def __str__(self):
     return self.board.__str__()
 
-if __name__ == '__main__':
-  connect_four = ConnectFour(Player('vince', 'A'), Player('jort', 'B'))
+if __name__ == "__main__":
+  connect_four = ConnectFour(Player("vince", 'A'), Player("jort", 'B'))
   print(connect_four.board)
   connect_four.move(0)
   connect_four.move(0)
