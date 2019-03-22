8c01661253a47af7d09b643fa32889b0c45de362
Jort van Gorkum <jortvangorkum@gmail.com>
2019-03-22 10:49:22 +0100

    Refactor, trained model and generated data

--- application/model_jort_play.py
+++ application/model_jort_play.py
@@ -6,15 +6,17 @@ from tensorflow import keras
 
 # Helper libraries
 import numpy as np
+from random import choice
 
 """
 Settings
 """
 MODEL_PATH = '../data/models'
-MODEL_NUMBER = 0
+MODEL_NUMBER = 20
 
-def predict_board(column:int, board: Board, model):
-  board = np.array([board.holes])
+def predict_board(column:int, board: Board, model, player: Player):
+  board = np.array([board.get_one_hot_array(player)])
+  board = np.reshape(board, (1, 6, 7, 3))
   prediction = model.predict(board)
   # Convert [[0.4]] -> 0.4
   prediction = prediction[0][0]
@@ -25,29 +27,48 @@ print("Start loading model")
 model = keras.models.load_model(f"{MODEL_PATH}/model_jort_{MODEL_NUMBER}.h5")
 print("Finished loading model")
 
-connect_four = ConnectFour()
-first_player = Player("Bot", 1)
-second_player = Player("Player", -1)
-current_player = first_player
+first_player = Player("Bot", 'B')
+second_player = Player("Player", 'S')
+connect_four = ConnectFour(first_player, second_player)
 
 """
 Game loop
 """
-while True:
-    
-  print(f"move from: {current_player.name}.")
-  if current_player is first_player:
-    possible_boards = connect_four.possible_boards(first_player)
-    (best_column, best_board) = max(possible_boards, key=lambda board: predict_board(*board, model))
-    connect_four.move(first_player, best_column)
-  else:
-    move = int(input("Input move: "))
-    connect_four.move(second_player, move)
+amount_of_games = 0
+amount_won_bot = 0
+amount_won_random = 0
 
-  print(connect_four.board)
-  
-  if (connect_four.has_won(current_player)):
-    print(f"{current_player.name} has won.")
-    break
+while(amount_of_games < 1000):
+  while True:
+    # print(f"move from: {connect_four.current_player.name}.")
+    if connect_four.current_player is first_player:
+      possible_boards_columns = connect_four.possible_boards_columns(first_player)
+      (best_column, best_board) = max(possible_boards_columns, key=lambda board_column: predict_board(*board_column, model, first_player))
+      # print(best_column)
+      connect_four.move(best_column)
+    else:
+      move = int(input("Input move: "))
+      # move = choice(connect_four.board.get_possible_columns())
+      connect_four.move(move)
+
+    print(connect_four.board)
     
-  current_player = first_player if current_player is second_player else second_player
+    won_player = connect_four.has_won()
+    if (won_player):
+      # print(f"{won_player.name} has won.")
+      if (won_player == first_player):
+        amount_won_bot += 1
+      else:
+        amount_won_random += 1
+      connect_four.reset()
+      break
+    elif(connect_four.is_draw()):
+      # print("It's a draw")
+      connect_four.reset()
+      break
+  
+  amount_of_games += 1
+  if(amount_of_games % 100 == 0):
+    print(f"Amount of Games: {amount_of_games}. Games won by bot: {((amount_won_bot/amount_of_games)*100):.2f}%. Games won by random: {((amount_won_random/amount_of_games)*100):.2f}%.")
+
+    
---
+++ data/models/info.md
@@ -0,0 +1,153 @@
+Model 0:
+    4 hidden layers
+    100.000 games
+    epochs 10
+    batch size 128
+    
+    Won: 61.2%
+Model 1:
+    6 hidden layers
+    100.000 games
+    epochs 10
+    batch size 128
+
+    Won: 57%
+Model 2:
+    2 hidden layers
+    100.000 games
+    epochs 10
+    batch size 128
+
+    Won: 53%
+Model 3:
+    2 hidden layers
+    10.000 games
+    epochs 10
+    batch size 128
+
+    Won: 57%
+Model 4:
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 128
+
+    Won: 60.6%
+Model 5:
+    6 hidden layers
+    10.000 games
+    epochs 10
+    batch size 128
+
+    Won: 52.6%
+Model 6:
+    6 hidden layers
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 53.4%
+Model 7:
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 53%
+Model 8:
+    4 hidden layers
+    2 Dropout layers of 0.5
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 53.4%
+Model 9:
+    3 hidden layers
+    10.000 games
+    epochs 30
+    batch size 32
+
+    Won: 52.7%
+Model 10:
+    AdamOptimizer
+    4 hidden layers
+    10.000 games
+    epochs 30
+    batch size 32
+
+    Won: 49.4%
+Model 11:
+    AdamOptimizer
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 45.5%
+Model 12:
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 20
+
+    Won: 42.6%
+Model 13:
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 5
+
+    Won: 38.9%
+Model 14:
+    kernal_regularizer lr=0.01
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 81%, because it just pust them in the left column, because every board returns 0.5
+Model 15:
+    activity_regularizer lr=0.01
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 77.6%, because it just pust them in the left column, because every board returns 0.5
+Model 16:
+    bias_regularizer lr=0.01
+    4 hidden layers
+    10.000 games
+    epochs 10
+    batch size 32
+
+    Won: 50.1%
+Model 17:
+    4 hidden layers
+    100.000 games
+    epochs 10
+    batch size 256
+
+    Won: 46.3%
+Model 18:
+    4 hidden layers
+    100.000 games
+    epochs 30
+    batch size 256
+
+    Won: 52.1%
+Model 19:
+    4 hidden layers
+    100.000 games
+    epochs 10
+    batch size 32
+
+    Won: 77%, because it just pust them in the left column, because every board returns 0.5
+Model 19:
+    4 hidden layers
+    100.000 games
+    epochs 10
+    batch size 64
+
+    Won: 58.7%
