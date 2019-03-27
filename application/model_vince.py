8e1db51facd9282ba6083e3efbf03ba5a5d2a686
Vince van Noort <vincevannoort@gmail.com>
2019-03-27 11:18:59 +0100

    updated models

--- application/model_vince.py
+++ application/model_vince.py
@@ -3,6 +3,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
 from ast import literal_eval
+from random import shuffle
 
 width = 7
 height = 6
@@ -13,10 +14,11 @@ inputs = width * height * 3
 Parse data
 """
 print(f'parse data.')
-with open('../data/data_generated/data_row_unbiased_classify_connect_four_game_5_overfit.txt') as file:
+with open('../data/data_generated/data_row_unbiased_classify_connect_four_game_100_overfit.txt') as file:
     content = file.readlines()
 
 data = [line.strip().split(";") for line in content[1:]] 
+shuffle(data)
 train_data = np.array([ np.fromstring(data_item[0][1:-1], dtype=int, sep=' ') for data_item in data ])
 train_labels = np.array([ np.fromstring(data_item[2][1:-1], sep=' ') for data_item in data ])
 
@@ -28,9 +30,9 @@ class Connect4KerasModel:
     self.model = Sequential([
       Dense(inputs, input_shape=(inputs,)),
       Activation('relu'),
-      Dense(inputs),
+      Dense(inputs * 5),
       Activation('relu'),
-      Dense(inputs),
+      Dense(inputs * 2),
       Activation('relu'),
       Dense(width),
       Activation('softmax'),
@@ -40,13 +42,13 @@ class Connect4KerasModel:
     # top_k_categorical_accuracy
 
   def train(self, train_data, train_labels):
-    self.model.fit(train_data, train_labels, epochs=15, batch_size=32)
+    self.model.fit(train_data, train_labels, epochs=32, batch_size=32)
 
 
 print(f'data & model ready, start training.')
 connect_four_model = Connect4KerasModel()
 connect_four_model.train(train_data, train_labels)
-connect_four_model.model.save('connect_four_model_vince.h5')
+connect_four_model.model.save('connect_four_model_vince_unbiased.h5')
 input = train_data[0]
 prediction = connect_four_model.model.predict(np.array([input,]))
 print(prediction)
