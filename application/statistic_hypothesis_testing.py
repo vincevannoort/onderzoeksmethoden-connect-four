29b99b5bed64298fc240bd129fbaa9fe12ba3f9b
Jort van Gorkum <jortvangorkum@gmail.com>
2019-04-03 13:15:06 +0200

    Made method for hypothesis testing

--- application/statistic_hypothesis_testing.py
+++ application/statistic_hypothesis_testing.py
@@ -3,50 +3,56 @@ from scipy import stats
 import random
 import math
 
-"""
-Hypothesis testing
-H0: meanj == meanv
-Ha: meanj > meanv
-"""
+def hypothesis_testing(samples1, samples2):
+  """
+  Hypothesis testing
+  H0: meanj == meanv
+  Ha: meanj > meanv
+  """
+  n1 = len(samples1)
+  n2 = len(samples2)
+
+  """
+  Own version
+  """
+  # Calculate mean
+  mean1 = stats.hmean(samples1)
+  mean2 = stats.hmean(samples2)
+
+  # Calculate sample variance
+  s1 = stats.tvar(samples1)
+  s2 = stats.tvar(samples2)
+
+  # Calculate standard error
+  se = math.sqrt(sj/nj + sv/nv)
+
+  # Calculate z-score
+  zscore = (meanj - meanv) / se
+
+  # Calculate p-value
+  # Confidence interval = 95%
+  pvalue = stats.norm.sf(abs(zscore))
+
+  print(zscore)
+  print(pvalue)
+
+  """
+  Statsmodels
+  """
+  # Confidence interval = 95%
+  # Returns pvalue = 1.0 when zscore is negative
+  # (ztest, pvalue) = stests.ztest(arrayj, x2=arrayv, value=0, alternative='larger')
+  # print(ztest, pvalue)
+
+  if(pvalue < 0.05):
+    print('Null hypothesis rejected')
+  else:
+    print('Null hypothesis not rejected')
+
 
 if __name__ == '__main__':
-    arrayj = [random.randint(700, 800) for _ in range(100)]
-    arrayv = [random.randint(800, 900) for _ in range(100)]
-    nj = len(arrayj)
-    nv = len(arrayv)
+  samples1 = [random.randint(700, 800) for _ in range(100)]
+  samples2 = [random.randint(800, 900) for _ in range(100)]
+  hypothesis_testing(samples1, samples2)
 
-    """
-    Own version
-    """
-    # Calculate mean
-    meanj = stats.hmean(arrayj)
-    meanv = stats.hmean(arrayv)
-
-    # Calculate sample variance
-    sj = stats.tvar(arrayj)
-    sv = stats.tvar(arrayv)
-
-    # Calculate standard error
-    se = math.sqrt(sj/nj + sv/nv)
-
-    # Calculate z-score
-    zscore = (meanj - meanv) / se
-
-    # Calculate p-value
-    # Confidence interval = 95%
-    pvalue = stats.norm.sf(zscore)
-
-    print(zscore)
-    print(pvalue)
-
-    """
-    Statsmodels
-    """
-    # Confidence interval = 95%
-    (ztest, pvalue) = stests.ztest(arrayj, x2=arrayv, value=0, alternative='larger')
-    print(ztest, pvalue)
-
-    if(pvalue < 0.05):
-        print('Null hypothesis rejected')
-    else:
-        print('Null hypothesis not rejected')
+    
