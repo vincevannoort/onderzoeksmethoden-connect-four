7685f77a208cb36d995c2aeebe22f48568a6ba04
Vince van Noort <vincevannoort@gmail.com>
2019-04-12 14:35:03 +0200

    images

--- application/statistic_visualisation.py
+++ application/statistic_visualisation.py
@@ -16,7 +16,7 @@ from matplotlib import pyplot as plt
 import matplotlib.style as style
 
 if __name__ == '__main__':
-  with open(f'../statistics/dataframes/analysis_10.pickle', 'rb') as dataframe_file:
+  with open(f'../statistics/dataframes/analysis_20.pickle', 'rb') as dataframe_file:
     correctness_per_player_data = pickle.load(dataframe_file)
   correctness_per_player_data = correctness_per_player_data.replace('model_jort', 'Win/Lose classifier')
   correctness_per_player_data = correctness_per_player_data.replace('model_vince', 'Column choice classifier')
@@ -26,7 +26,7 @@ if __name__ == '__main__':
   # size (resolution)
   style.use('seaborn-poster')
 
-  plot = sns.barplot(x="Player", y="Winning moves", palette=["#63b7ff"] * 10 + ["#00844a"] * 10, data=pd.concat([correctness_per_player_data[:10], correctness_per_player_data[10:]]))
+  plot = sns.barplot(x="Player", y="Winning moves", palette=["#63b7ff"] * 10 + ["#00844a"] * 10, data=pd.concat([correctness_per_player_data[:10], correctness_per_player_data[20:30]]))
   plot.set(xlabel="Classifier", ylabel="Correct winning moves (out of 1000) per player")
   plot.figure.savefig(f"../statistics/images/analysis-winning-moves-per-player.png")
   plot.figure.clf()
@@ -35,25 +35,25 @@ if __name__ == '__main__':
   plot.set(xlabel="Classifier", ylabel="Correct winning moves (out of 1000)")
   plot.figure.savefig(f"../statistics/images/analysis-winning-moves.png")
   plot.figure.clf()
-  hypothesis_testing(list(correctness_per_player_data["Winning moves"][:10]), list(correctness_per_player_data["Winning moves"][10:]))
+  hypothesis_testing(list(correctness_per_player_data["Winning moves"][:20]), list(correctness_per_player_data["Winning moves"][20:]))
 
   plot = sns.boxplot(x="Type", y="Blocking moves", data=correctness_per_player_data)
   plot.set(xlabel="Classifier", ylabel="Correct blocking moves (out of 1000)")
   plot.figure.savefig(f"../statistics/images/analysis-blocking-moves.png")
   plot.figure.clf()
-  hypothesis_testing(list(correctness_per_player_data["Blocking moves"][:10]), list(correctness_per_player_data["Blocking moves"][10:]))
+  hypothesis_testing(list(correctness_per_player_data["Blocking moves"][:20]), list(correctness_per_player_data["Blocking moves"][20:]))
 
   plot = sns.boxplot(x="Type", y="Won against random", data=correctness_per_player_data)
   plot.set(xlabel="Classifier", ylabel="Won against random (out of 100)")
   plot.figure.savefig(f"../statistics/images/analysis-won-against-random.png")
   plot.figure.clf()
-  hypothesis_testing(list(correctness_per_player_data["Won against random"][:10]), list(correctness_per_player_data["Won against random"][10:]))
+  hypothesis_testing(list(correctness_per_player_data["Won against random"][:20]), list(correctness_per_player_data["Won against random"][20:]))
 
   plot = sns.boxplot(x="Type", y="Won against opposite", data=correctness_per_player_data)
   plot.set(xlabel="Classifier", ylabel="Won against opposite (out of 100)")
   plot.figure.savefig(f"../statistics/images/analysis-won-against-opposite.png")
   plot.figure.clf()
-  hypothesis_testing(list(correctness_per_player_data["Won against opposite"][:10]), list(correctness_per_player_data["Won against opposite"][10:]))
+  hypothesis_testing(list(correctness_per_player_data["Won against opposite"][:20]), list(correctness_per_player_data["Won against opposite"][20:]))
 
   won_against_random = list(correctness_per_player_data['Won against random'])
   plot = sns.boxplot(x="Type", y="Average moves played random", data=correctness_per_player_data)
